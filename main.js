2570291b5ee35a3af9a9f0c4b6261709af948875
Your Name <sahiledu2007@gmail.com>
2026-03-16 04:43:48 +0600

    Solve the nav bar issue

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 const text = "Future Software Developer";
-
 let index = 0;
 
 function type() {
+    const typingElement = document.getElementById("typing");
+
+    if (!typingElement) return; // stop if element doesn't exist
+
     if (index < text.length) {
-        document.getElementById("typing").textContent += text.charAt(index);
+        typingElement.textContent += text.charAt(index);
         index++;
         setTimeout(type, 70);
     }
 }
 
 type();
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav-links");
+
+if(toggle && nav){
+    toggle.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+}
+
+});
